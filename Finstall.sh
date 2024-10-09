2802ad1a0f7051c13a699b77364faa24426071d3
CYPRESSEN <149873042+ZYPRESSEN-AND-OSPREY@users.noreply.github.com>
2024-10-09 20:14:53 +0800

    Update Finstall.sh

--- Finstall.sh
+++ Finstall.sh
@@ -20,6 +20,7 @@ configure_dnf() {
 configure_repos() {
 	sudo sed -i 's|^metalink=|#metalink=|g' /etc/yum.repos.d/fedora*.repo
 	sudo sed -i 's|^#baseurl=http://download.example/pub/fedora/linux|baseurl=https://mirrors.ustc.edu.cn/fedora|g' /etc/yum.repos.d/fedora*.repo
+	sudo sed -i 's|enabled=1|enabled=0|g' /etc/yum.repos.d/fedora-cisco-openh264.repo
 }
 
 install_rpmfusion() {
