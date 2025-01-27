9412b5bab20f4b231d515412f515e29da30c98ab
CYPRESSEN <149873042+CZYPRESSEN@users.noreply.github.com>
2025-01-27 17:20:38 +0800

    Update R_INSTALL.sh

--- R_INSTALL.sh
+++ R_INSTALL.sh
@@ -6,8 +6,8 @@ trap 'echo "Error occurred at line $LINENO. Command: $BASH_COMMAND"' ERR
 
 # 检查root权限
 check_root() {
-    if [ "$(id -u)" -ne 0 ]; then
-        echo "请使用sudo运行此脚本"
+    if [ "$(whoami)" != "root" ]; then
+        echo "请使用 root 用户运行此脚本"
         exit 1
     fi
 }
